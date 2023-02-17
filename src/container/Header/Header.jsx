6799296d778addeed6cf2545c91ece9bb15ad07e
elmo24-design <mahupil24@gmail.com>
2023-02-17 14:53:21 +0800

    Changed hero image section to vexel and image in the about section to chibi

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,38 +1,38 @@
-import React from 'react'
-import {motion} from 'framer-motion'
+import React from "react";
+import { motion } from "framer-motion";
 
-import {AppWrap} from '../../wrapper'
-import {images} from '../../constants'
-import './Header.scss'
+import { AppWrap } from "../../wrapper";
+import { images } from "../../constants";
+import "./Header.scss";
 
-import { AiOutlineDownCircle } from 'react-icons/ai'
+import { AiOutlineDownCircle } from "react-icons/ai";
 // import Typed from 'react-typed'
 // import Typewriter from 'typewriter-effect';
 
 const scaleVariants = {
   whileInView: {
-    scale: [0,1],
-    opacity: [0,1],
+    scale: [0, 1],
+    opacity: [0, 1],
     transition: {
       duration: 1,
-      ease: 'easeInOut'
-    }
-  }
-}
+      ease: "easeInOut",
+    },
+  },
+};
 
 const Header = () => {
   return (
-    <div className='app__flex'>
-      <div className='app__header'>
+    <div className="app__flex">
+      <div className="app__header">
         {/* First div */}
         <motion.div
-          whileInView={{ x: [-100, 0], opacity: [0, 1]}}
+          whileInView={{ x: [-100, 0], opacity: [0, 1] }}
           transition={{ duration: 1.1 }}
           className="app__header-info"
         >
           <div className="app__header-heading">
-            <h1 className='heading-text'>HI, I'M ELMO</h1>
-            <p className='heading-desc'>
+            <h1 className="heading-text">HI, I'M ELMO</h1>
+            <p className="heading-desc">
               {/* <Typewriter
                 options={{
                   strings: ['Fullstack Developer', 'Frontend Developer'],
@@ -43,7 +43,8 @@ const Header = () => {
               /> */}
             </p>
             <p className="heading-desc-2">
-              A Fullstack developer from Pililla, Rizal, Philippines. I craft creative and engaging Web Apps
+              A Fullstack developer from Pililla, Rizal, Philippines. I craft
+              creative and engaging Web Apps
             </p>
           </div>
           {/* Call to action buttons  */}
@@ -52,47 +53,49 @@ const Header = () => {
 
             <motion.div
               initial={{ opacity: 0 }}
-              animate={{ opacity: 1,  transition: {
-                  delay: 1.5
-              } }}
+              animate={{
+                opacity: 1,
+                transition: {
+                  delay: 1.5,
+                },
+              }}
               className="arrow-down"
             >
               <AiOutlineDownCircle />
-          </motion.div>
+            </motion.div>
           </div>
-          
         </motion.div>
 
         {/* Second div  */}
         <motion.div
-            whileInView={{ x: [100, 0], opacity: [0, 1]}}
-            transition={{ duration: 1.1 }}
-            className="app__header-img"
+          whileInView={{ x: [100, 0], opacity: [0, 1] }}
+          transition={{ duration: 1.1 }}
+          className="app__header-img"
         >
           <div>
-            <img src={images.chibielms2} alt="profile_bg" className='chibi' />
+            <img
+              src={images.vexelNew}
+              alt="profile_bg"
+              className="vexelFinal"
+            />
           </div>
-          
+
           <motion.div
             variant={scaleVariants}
             whileInView={scaleVariants.whileInView}
             className="app__header-circles"
           >
             <div className="circle-container circle-container-1">
-              <div className='app__flex circle circle-1'>
-                <img src={images.uiLogo} alt="ui logo" className='ui-logo' />
+              <div className="app__flex circle circle-1">
+                <img src={images.uiLogo} alt="ui logo" className="ui-logo" />
               </div>
-              <div className="white-box">
-                UI Designer
-              </div>
+              <div className="white-box">UI Designer</div>
             </div>
             <div className="circle-container circle-container-2">
-              <div className='app__flex circle circle-2'>
-                <img src={images.code} alt="code" className='code' />
+              <div className="app__flex circle circle-2">
+                <img src={images.code} alt="code" className="code" />
               </div>
-              <div className="white-box">
-                Developer
-              </div>
+              <div className="white-box">Developer</div>
             </div>
           </motion.div>
           {/* <motion.img
@@ -104,10 +107,9 @@ const Header = () => {
           >
           </motion.img> */}
         </motion.div>
-      
-        </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default AppWrap(Header, 'home', 'app__primarybg')
+export default AppWrap(Header, "home", "app__primarybg");
